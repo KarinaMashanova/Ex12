63beff5712a333985bb6531ee8ecc22d43499a9f
Karina Mashanova <karmen.01@inbox.ru>
2021-06-08 22:33:59 +0300

    Test

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -1,52 +1,50 @@
-// Copyright 2021 GHA Test Team
-
+// Copyright 2021 KarMashanova
 #ifndef INCLUDE_TIMEDDOOR_H_
 #define INCLUDE_TIMEDDOOR_H_
+#include <iostream>
 
 class DoorTimerAdapter;
 class Timer;
 class Door;
 class TimedDoor;
-
 class TimerClient {
  public:
   virtual void Timeout() = 0;
 };
-
 class Door {
  public:
   virtual void lock() = 0;
   virtual void unlock() = 0;
   virtual bool isDoorOpened() = 0;
 };
-
 class DoorTimerAdapter : public TimerClient {
  private:
   TimedDoor& door;
+
  public:
   explicit DoorTimerAdapter(TimedDoor&);
   void Timeout();
 };
-
 class TimedDoor : public Door {
  private:
-  DoorTimerAdapter * adapter;
+  DoorTimerAdapter* adapter;
   int iTimeout;
   bool opened;
+
  public:
   explicit TimedDoor(int);
-  bool isDoorOpened();
-  void unlock();
-  void lock();
+  bool isDoorOpened() override;
+  void unlock() override;
+  void lock() override;
   void DoorTimeOut();
   void throwState();
 };
 
 class Timer {
-  TimerClient *client;
+  TimerClient* client;
   void sleep(int);
+
  public:
   void tregister(int, TimerClient*);
 };
-
 #endif  // INCLUDE_TIMEDDOOR_H_
---
+++ src/TimedDoor.cpp
@@ -0,0 +1,43 @@
+// Copyright 2021 KarMashanova
+#include "TimedDoor.h"
+
+#include <iostream>
+#include <string>
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& door_) : door(door_) {}
+
+TimedDoor::TimedDoor(int _t) {
+  (*this).lock();
+  (*this).iTimeout = _t;
+  (*this).adapter = new DoorTimerAdapter(*this);
+}
+
+void DoorTimerAdapter::Timeout() { door.DoorTimeOut(); }
+
+bool TimedDoor::isDoorOpened() { return opened; }
+void TimedDoor::unlock() {
+  opened = true;
+  Timer n;
+  n.tregister((*this).iTimeout, (*this).adapter);
+}
+void TimedDoor::lock() { opened = false; }
+void TimedDoor::DoorTimeOut() {
+  throw("close the door!");
+}
+void TimedDoor::throwState() {
+  if (!isDoorOpened()) {
+    throw("the door is closed!");
+  } else {
+    throw("the door is opened!");
+  }
+}
+void Timer::sleep(int _t) {
+  time_t beg = time(nullptr);
+  while (time(nullptr) - beg < _t) {
+    continue;
+  }
+}
+void Timer::tregister(int c, TimerClient* client) {
+  sleep(c);
+  (*client).Timeout();
+}
